feb906d47c250f0eaeab14343e00deeb878f6d7a
mitchelle <mitchellenjoki7@gmail.com>
2022-07-02 20:56:55 +0300

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
-function addingEventListener() {  input.addEventListener('click', function(event) {
-    return "clicked"
-  });
+// const input = document.getElementById('input')
+// input.addEventListener('click' , function (){
+//    return alert('I was clicked!')
+// }) 
+
+// const addingEventListener = document.getElementById('input')
+// input.addEventListener('click', =>
+//     alert('I was clicked!')
+// ) 
+
+const input = document.getElementById ('input')
+
+function addingEventListener(){
+    input.addEventListener('click' , function (e){
+        return" i was clicked!"
+    })
 }
